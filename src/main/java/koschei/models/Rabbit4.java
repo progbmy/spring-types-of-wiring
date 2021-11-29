2fa72a664ecd41e27fa93d2f7bfd98346006808f
MIhail <skuter63@mail.ru>
2021-11-29 22:21:57 +0400

    Egg6 update

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,9 +5,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
+    private final Duck5 duck;
+
+    @Autowired
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
+
 
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
     }
 }
